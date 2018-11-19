8013d00c4a7400ce4959b5c19ede97c3ac501930
ivan4000 <sxln1@mail.ru>
2018-11-20 01:08:10 +0300

    JL208javaBean

---
+++ src/com/Main.java
@@ -0,0 +1,31 @@
+package com;
+
+import bumpersticker.BumperSticker;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    static JFrame jFrame = getFrame();
+    static JPanel jPanel = new JPanel();
+
+    public static void main(String[] args) {
+        jFrame.add(jPanel);
+        FaceBean fb=new FaceBean();
+        fb.setMouthWidth(100);
+        fb.frown();
+        jFrame.add(fb);
+        jFrame.revalidate();
+    }
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame() {
+        };
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width / 2 - 250, dimension.height / 2 - 150, 500, 500);
+        return jFrame;
+
+    }
+}
